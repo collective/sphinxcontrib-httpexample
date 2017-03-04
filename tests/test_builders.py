704caaf378034483a884c9a01a9e624f28342416
Asko Soukka <asko.soukka@iki.fi>
2017-03-05 01:21:22 +0200

    Move command fixtures into files

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -5,42 +5,24 @@ from sphinxcontrib.httpexample.builders import build_httpie_command
 from sphinxcontrib.httpexample.builders import build_requests_command
 
 from .test_fixtures import FIXTURE_001_REQUEST
+from .test_fixtures import FIXTURE_001_CURL
+from .test_fixtures import FIXTURE_001_HTTPIE
+from .test_fixtures import FIXTURE_001_REQUESTS
 
 
 def test_curl_fixture_001():
     request = parse_request(FIXTURE_001_REQUEST)
     command = build_curl_command(request)
-    assert command == (
-        'curl '
-        '-i '
-        '-X POST http://localhost:8080/Plone/folder '
-        '-H "Accept: application/json" '
-        '-H "Content-Type: application/json" '
-        '--data-raw \'{"@type": "Document", "title": "My Document"}\' '
-        '--user admin:admin '
-    ).strip()
+    assert command == FIXTURE_001_CURL.decode('utf-8')
 
 
 def test_httpie_fixture_001():
     request = parse_request(FIXTURE_001_REQUEST)
     command = build_httpie_command(request)
-    assert command == (
-        'http '
-        'POST '
-        'http://localhost:8080/Plone/folder '
-        '\\\\@type=Document title=My Document '
-        '-a admin:admin '
-    ).strip()
+    assert command == FIXTURE_001_HTTPIE.decode('utf-8')
 
 
 def test_requests_fixture_001():
     request = parse_request(FIXTURE_001_REQUEST)
     command = build_requests_command(request)
-    assert command == (
-        'requests.post('
-        "'http://localhost:8080/Plone/folder', "
-        "headers={'Accept': 'application/json'}, "
-        "json={'@type': 'Document', 'title': 'My Document'}, "
-        "auth=('admin', 'admin')"
-        ')'
-    ).strip()
+    assert command == FIXTURE_001_REQUESTS.decode('utf-8')
--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -4,11 +4,19 @@ import os
 
 def read_fixture(filename):
     with open(os.path.join(os.path.dirname(__file__), 'fixtures', filename)) as fp:  # noqa
-        return (lambda s: isinstance(s, bytes) and s or s.encode('utf-8'))(fp.read())  # noqa
+        return (lambda s: isinstance(s, bytes) and s or s.encode('utf-8'))(fp.read().strip())  # noqa
 
 
 FIXTURE_001_REQUEST = read_fixture('001.request.txt')
+FIXTURE_001_CURL = read_fixture('001.curl.txt')
+FIXTURE_001_HTTPIE = read_fixture('001.httpie.txt')
+FIXTURE_001_REQUESTS = read_fixture('001.requests.txt')
+FIXTURE_001_RESPONSE = read_fixture('001.response.txt')
 
 
 def test_fixtures():
     assert isinstance(FIXTURE_001_REQUEST, bytes)
+    assert isinstance(FIXTURE_001_CURL, bytes)
+    assert isinstance(FIXTURE_001_HTTPIE, bytes)
+    assert isinstance(FIXTURE_001_REQUESTS, bytes)
+    assert isinstance(FIXTURE_001_RESPONSE, bytes)
