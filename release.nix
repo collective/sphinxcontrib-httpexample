277f3a973801700abc53555eff8e089d1dacdc07
Asko Soukka <asko.soukka@iki.fi>
2018-07-22 17:09:39 +0300

    Pin nixpkgs for release.nix (still used for docs)

--- release.nix
+++ release.nix
@@ -1,4 +1,8 @@
-{ pkgs ? import <nixpkgs> {}
+{ pkgs ? import (builtins.fetchTarball {
+    # update sha256 with value of "nix-prefetch-url --unpack [url]"
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/d6c6c7fcec6dbd2b8ab14f0b35d56c7733872baa.tar.gz";
+    sha256 = "1bq59575b58lmh5w8bh7pwp17c3p2nm651bz1i3q224c4zsj9294";  # nixos-18.03 at 2018-07-22
+  }) {}
 , supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
 , supportedPythons ? [ "python2" "python3" ]
 , setup ? import ./setup.nix
