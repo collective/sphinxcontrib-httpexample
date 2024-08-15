8ec736be9df844da2d84a56f0180d4fe0d648139
Asko Soukka <asko.soukka@iki.fi>
2024-08-16 00:52:24 +0300

    Cleanup

--- default.nix
+++
@@ -1,7 +0,0 @@
-{ system ? builtins.currentSystem
-, pkgs ? import ../../nix { nixpkgs = sources."nixpkgs-20.09"; inherit system; }
-, sources ? import ../../nix/sources.nix
-, setup ? import ./setup.nix { inherit pkgs; }
-}:
-
-setup.build
